c9451829c5a87005929ad03cb03fdea476fb96d4
Mateus Gomes <mateusgomesc7@gmail.com>
2020-02-06 09:59:18 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
+import Tabela from './Tabela';
+import Formulario from './Formulario';
+import Header from './Header';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    autores: [
+      {
+        nome: 'Paulo',
+        livro: 'React',
+        preco: '1000'
+      },
+      {
+        nome: 'Daniel',
+        livro: 'Java',
+        preco: '99'
+      },
+      {
+        nome: 'Marcos',
+        livro: 'Design',
+        preco: '150'
+      },
+      {
+        nome: 'Bruno',
+        livro: 'DevOps',
+        preco: '100'
+      },
+      {
+        nome: 'Nico',
+        livro: 'Java',
+        preco: '9999'
+      }
+    ],
+  };
+
+  removeAutor = index => {
+    const { autores } = this.state;
+
+    this.setState(
+      {
+        autores: autores.filter((autor, posAtual) => {
+          return posAtual !== index;
+        }),
+      }
+    );
+  }
+
+  escutadorDeSubmit = autor => {
+    this.setState({ autores: [...this.state.autores, autor] });
+  }
+
+  render() {
+    return (
+      <>
+        <Header />
+        <div className="container mb-10">
+          <h1>Lista e Formulário</h1>
+          <Tabela autores={this.state.autores} removeAutor={this.removeAutor} />
+          <Formulario escutadorDeSubmit={this.escutadorDeSubmit} />
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
